dfab7e1b20b4080abc2430797ef8a9f4e5ad565c
loveziyan0124 <113508541+loveziyan0124@users.noreply.github.com>
2023-06-20 15:54:17 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
-<template>
-  <h3>模板语法</h3>
-  <p>{{ msg }}</p>
-  <p>{{ num + 1 }}</p>
-  <p>{{ ok ? "YES" :"NO" }}</p>
-  <p>{{ message.split("").reverse().join("") }}</p>
-  <p>{{ rawHtml }}</p>
-  <p v-html="rawHtml"></p>
-</template>
-
-<script>
-export default {
-  data(){
-    return{
-      msg:"神奇的语法",
-      num:10,
-      ok:true,
-      message:"大家好",
-      rawHtml:"<a href='https://www.baidu.com'>百度网页链接</a>"
-    }
-  }
-}
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import IfDemo from './components/ifDemo.vue'
+import ListDemo from './components/listDemo.vue'
+import Key from './components/key.vue'
 </script>
 
+<template>
+  <HelloWorld/>
+  <IfDemo/>
+  <ListDemo/>
+  <Key/>
+</template>
---
+++ src/components/key.vue
@@ -0,0 +1,37 @@
+<template>
+    <h3>key属性添加到v-for中</h3>
+    <P v-for="(item,index) of names" :key="index">{{ item }}</P>
+    <div v-for="item of result" :key="item.id">
+        <p>{{ item.title }}</p>
+        <img :src="item.avator" alt="">
+    </div>
+</template>
+<script>
+
+export default {
+    data(){
+        return{
+            names:["HTML","CSS","JAVASCRIPT"],
+            result:[
+                {
+                    "id":22654351,
+                    "title":"许昌 | 感受一座城市的璀璨夜景",
+                    "avator":"https://pic.q"
+                },
+                {
+                    "id":22658921,
+                    "title":"郑州 | 感受一座城市的璀璨夜景",
+                    "avator":"https://pic.q"
+                },
+                {
+                    "id":22653451,
+                    "title":"开封 | 感受一座城市的璀璨夜景",
+                    "avator":"https://pic.q"
+                },
+            ]
+        }
+    }
+}
+
+
+</script>
