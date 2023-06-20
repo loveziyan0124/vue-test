919c20e1c51c9049c2140512344884333cc1016c
loveziyan0124 <113508541+loveziyan0124@users.noreply.github.com>
2023-06-20 15:36:33 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>模板语法</h3>
+  <p>{{ msg }}</p>
+  <p>{{ num + 1 }}</p>
+  <p>{{ ok ? "YES" :"NO" }}</p>
+  <p>{{ message.split("").reverse().join("") }}</p>
+  <p>{{ rawHtml }}</p>
+  <p v-html="rawHtml"></p>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:"神奇的语法",
+      num:10,
+      ok:true,
+      message:"大家好",
+      rawHtml:"<a href='https://www.baidu.com'>百度网页链接</a>"
+    }
+  }
+}
+</script>
+
